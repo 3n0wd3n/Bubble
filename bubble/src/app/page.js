c35649916b26576a19269f3946400d8afaae4268
Michael Hajný <CryptextCompany@gmail.com>
2025-09-26 17:30:12 +0200

    fix issue with width

--- bubble/src/app/page.js
+++ bubble/src/app/page.js
@@ -5,133 +5,146 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      {/* header */}
-      <header className={styles.header}>
-        {/* navigation */}
-        <nav className={styles.nav}>
-          <div className={styles.nav__container}>
-            {/* logo */}
-            <Link href='/' className='logo'>
-              <Image src='/logo_nav.png' alt='Bubble logo' width={48} height={48} />
-            </Link>
-            {/* links */}
-            <ul className={styles.nav__list}>
-              <li className={styles.nav__item}>
-                <a href='#'>Really? Another Social App</a>
-              </li>
-              <li className={styles.nav__item}>
-                <a href='#'>How To Master Bubble</a>
-              </li>
-              <li className={styles.nav__item}>
-                <a href='#'>F&Q</a>
-              </li>
-              <li className={styles.nav__item}>
-                <a href='#'>Partners</a>
-              </li>
-            </ul>
-          </div>
-          {/* buttons */}
-          <div className={styles.nav__buttons}>
-            <a href='#' className={`${styles.nav__button} ${styles["nav__button--ghost"]}`}>
-              Advertise on Bubble
-            </a>
-            <a href='#' className={`${styles.nav__button} ${styles["nav__button--filled"]}`}>
-              Get the app
-            </a>
-          </div>
-        </nav>
-      </header>
+      {/* general container */}
+      <div className={`${styles["container--header"]}`}>
+        {/* header */}
+        <header className={styles.header}>
+          {/* navigation */}
+          <nav className={styles.nav}>
+            <div className={styles.nav__container}>
+              {/* logo */}
+              <Link href='/' className='logo'>
+                <Image src='/logo_nav.png' alt='Bubble logo' width={48} height={48} />
+              </Link>
+              {/* links */}
+              <ul className={styles.nav__list}>
+                <li className={styles.nav__item}>
+                  <a href='#'>Really? Another Social App</a>
+                </li>
+                <li className={styles.nav__item}>
+                  <a href='#'>How To Master Bubble</a>
+                </li>
+                <li className={styles.nav__item}>
+                  <a href='#'>F&Q</a>
+                </li>
+                <li className={styles.nav__item}>
+                  <a href='#'>Partners</a>
+                </li>
+              </ul>
+            </div>
+            {/* buttons */}
+            <div className={styles.nav__buttons}>
+              <a href='#' className={`${styles.nav__button} ${styles["nav__button--ghost"]}`}>
+                Advertise on Bubble
+              </a>
+              <a href='#' className={`${styles.nav__button} ${styles["nav__button--filled"]}`}>
+                Get the app
+              </a>
+            </div>
+          </nav>
+        </header>
+      </div>
       {/* main */}
       <main className={styles.main}>
         {/* intro */}
         <section className={styles.intro}>
-          <div className={styles.intro__content}>
-            <div className={styles.intro__heading_container}>
-              <span>Bubble.</span>
-              <h1>
-                A new <span className={`${styles["outlined-text"]}`}>social network</span> where photos and videos are
-                tied to real-world places
-              </h1>
+          {/* general container */}
+          <div className={`${styles["container--intro"]}`}>
+            <div className={styles.intro__content}>
+              <div className={styles.intro__heading_container}>
+                <span>Bubble.</span>
+                <h1>
+                  A new <span className={`${styles["outlined-text"]}`}>social network</span> where photos and videos are
+                  tied to real-world places
+                </h1>
+              </div>
+              <p>
+                Leave photos or videos at real locations and create stories waiting to be discovered. Every place
+                becomes a canvas for your most meaningful moments.
+              </p>
+              <div className={styles.intro__buttons}>
+                <a href='#' className={`${styles.intro__button} ${styles["intro__button--filled"]}`}>
+                  Get the app
+                </a>
+                <a href='#' className={`${styles.intro__button} ${styles["intro__button--ghost"]}`}>
+                  Advertise on Bubble
+                </a>
+              </div>
             </div>
-            <p>
-              Leave photos or videos at real locations and create stories waiting to be discovered. Every place becomes
-              a canvas for your most meaningful moments.
-            </p>
-            <div className={styles.intro__buttons}>
-              <a href='#' className={`${styles.intro__button} ${styles["intro__button--filled"]}`}>
-                Get the app
-              </a>
-              <a href='#' className={`${styles.intro__button} ${styles["intro__button--ghost"]}`}>
-                Advertise on Bubble
-              </a>
+            <div className={styles.image}>
+              <Image
+                src='/intro_image.png'
+                alt='Intro image'
+                width={619}
+                height={613}
+                style={{ width: "100%", height: "auto" }}
+              />
             </div>
-          </div>
-          <div className={styles.image}>
-            <Image
-              src='/intro_image.png'
-              alt='Intro image'
-              width={619}
-              height={613}
-              style={{ width: "100%", height: "auto" }}
-            />
           </div>
         </section>
         {/* about - why would they need us */}
         <section className={styles.about}>
-          <h2>Why would you need another social network? </h2>
-          <div className={styles.about__container}>
-            <article className={styles.about__item}>
-              <Image src='/IoShareSocialOutline.svg' alt='Share icon' width={48} height={48} />
-              <div className={styles.item__text_container}>
-                <p>
-                  Allows you to share your memories in a unique way with friends, followers, or keep them for yourself.
-                </p>
-                <p>Capture moments exactly where they happen and decide who can unlock them.</p>
-              </div>
-            </article>
-            <article className={styles.about__item}>
-              <Image src='/IoEarthOutline.svg' alt='Share icon' width={48} height={48} />
-              <div className={styles.item__text_container}>
-                <p>Lets you discover hidden stories left by others in real places.</p>
-                <p>Explore memories waiting to be found and uncover stories tied to meaningful locations.</p>
-              </div>
-            </article>
-            <article className={styles.about__item}>
-              <Image src='/IoSparklesOutline.svg' alt='Share icon' width={48} height={48} />
-              <div className={styles.item__text_container}>
-                <p>Brings excitement to your everyday life by exploring memories nearby</p>
-                <p>Leave surprises nearby and explore new spots to turn everyday places into adventures.</p>
-              </div>
-            </article>
-            <article className={styles.about__item}>
-              <Image src='/IoMap.svg' alt='Share icon' width={48} height={48} />
-              <div className={styles.item__text_container}>
-                <p>Transform space around you into unforgettable adventures</p>
-                <p>Create a digital time capsule that connects people through shared experiences and locations.</p>
-              </div>
-            </article>
+          {/* general container */}
+          <div className={`${styles["container--about"]}`}>
+            <h2>Why would you need another social network? </h2>
+            <div className={styles.about__container}>
+              <article className={styles.about__item}>
+                <Image src='/IoShareSocialOutline.svg' alt='Share icon' width={48} height={48} />
+                <div className={styles.item__text_container}>
+                  <p>
+                    Allows you to share your memories in a unique way with friends, followers, or keep them for
+                    yourself.
+                  </p>
+                  <p>Capture moments exactly where they happen and decide who can unlock them.</p>
+                </div>
+              </article>
+              <article className={styles.about__item}>
+                <Image src='/IoEarthOutline.svg' alt='Share icon' width={48} height={48} />
+                <div className={styles.item__text_container}>
+                  <p>Lets you discover hidden stories left by others in real places.</p>
+                  <p>Explore memories waiting to be found and uncover stories tied to meaningful locations.</p>
+                </div>
+              </article>
+              <article className={styles.about__item}>
+                <Image src='/IoSparklesOutline.svg' alt='Share icon' width={48} height={48} />
+                <div className={styles.item__text_container}>
+                  <p>Brings excitement to your everyday life by exploring memories nearby</p>
+                  <p>Leave surprises nearby and explore new spots to turn everyday places into adventures.</p>
+                </div>
+              </article>
+              <article className={styles.about__item}>
+                <Image src='/IoMap.svg' alt='Share icon' width={48} height={48} />
+                <div className={styles.item__text_container}>
+                  <p>Transform space around you into unforgettable adventures</p>
+                  <p>Create a digital time capsule that connects people through shared experiences and locations.</p>
+                </div>
+              </article>
+            </div>
           </div>
         </section>
         {/* usage - create bubble */}
         <section className={styles.create_section}>
-          <span className={styles.tagline}>01 Create memmories</span>
-          <div className={styles.create__content}>
-            <div className={styles.create__image}>
-              <Image
-                src='/create-image.png'
-                alt='Intro image'
-                width={600}
-                height={733}
-                style={{ width: "100%", height: "auto" }}
-              />
-            </div>
-            <div className={styles.create__container}>
-              <span>Capture</span>
-              <h2>Leave photos at specific locations</h2>
-              <p>
-                Select a meaningful location, upload your photo or video and select who can see your bubble. Your memory
-                becomes a hidden story waiting to be discovered by others.
-              </p>
+          {/* general container */}
+          <div className={`${styles["container--create"]}`}>
+            <span className={styles.tagline}>01 Create memmories</span>
+            <div className={styles.create__content}>
+              <div className={styles.create__image}>
+                <Image
+                  src='/create-image.png'
+                  alt='Intro image'
+                  width={600}
+                  height={733}
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+              <div className={styles.create__container}>
+                <span>Capture</span>
+                <h2>Leave photos at specific locations</h2>
+                <p>
+                  Select a meaningful location, upload your photo or video and select who can see your bubble. Your
+                  memory becomes a hidden story waiting to be discovered by others.
+                </p>
+              </div>
             </div>
           </div>
         </section>
